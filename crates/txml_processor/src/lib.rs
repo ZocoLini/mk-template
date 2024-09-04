d0089de6974268bb2030cd8c6de1b9b847861846
Borja Castellano <borjacastellano1@gmail.com>
2024-09-05 01:13:17 +0200

    (Feature) txml_processor can create from xml but can't create content in a directory.

---
+++ crates/txml_processor/src/lib.rs
@@ -0,0 +1,163 @@
+mod objects;
+
+use crate::objects::{AttributeHandler, Directory, File, TxmlStructure};
+use crate::TxmlProcessorError::{InvalidDirectory, InvalidTag, UnknownParseError};
+use quick_xml::events::Event;
+use quick_xml::Reader;
+use std::collections::VecDeque;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub enum TxmlProcessorError
+{
+    InvalidDirectory,
+    UnknownParseError,
+    InvalidTag,
+}
+
+pub fn process_txml(txml: &PathBuf) -> Result<TxmlStructure, TxmlProcessorError>
+{
+    let mut txml_structure = TxmlStructure::new();
+
+    if !txml.exists() {
+        return Err(InvalidDirectory);
+    }
+    if !txml.is_file() {
+        return Err(InvalidDirectory);
+    }
+
+    let mut reader = Reader::from_file(txml).expect("Should exist");
+    let mut event_buf = Vec::new();
+
+    let mut dir_queue: VecDeque<&mut Directory> = VecDeque::new();
+    let mut current_file: Option<File> = None;
+
+    loop {
+        match reader.read_event_into(&mut event_buf) {
+            Ok(Event::Start(ref e)) => match e.name().0 {
+                b"Root" => continue,
+                b"Directory" => {
+                    // TODO: Make it save inner Files
+
+                    let mut directory = Directory::new();
+
+                    e.attributes().for_each(|attr| {
+                        directory.process_attribute(attr.expect("Error reading attribute"));
+                    });
+
+                    if dir_queue.is_empty() {
+                        txml_structure.add_directory(directory)
+                    } else {
+                        dir_queue
+                            .back_mut()
+                            .expect("Shouldn't be empty")
+                            .add_directory(directory)
+                    }
+                }
+                b"File" => {
+                    current_file = Some(File::new());
+
+                    e.attributes().for_each(|attr| {
+                        current_file
+                            .as_mut()
+                            .unwrap()
+                            .process_attribute(attr.expect("Error reading attribute"));
+                    });
+                }
+                _ => return Err(InvalidTag),
+            },
+            Ok(Event::Empty(e)) => match e.name().0 {
+                b"Root" => continue,
+                b"Directory" => {
+                    let mut directory = Directory::new();
+                    e.attributes().for_each(|attr| {
+                        directory.process_attribute(attr.expect("Error reading attribute"));
+                    });
+
+                    if dir_queue.is_empty() {
+                        txml_structure.add_directory(directory)
+                    } else {
+                        dir_queue
+                            .back_mut()
+                            .expect("Shouldn't be empty")
+                            .add_directory(directory)
+                    }
+                }
+                b"File" => {
+                    let mut file = File::new();
+                    e.attributes().for_each(|attr| {
+                        file.process_attribute(attr.expect("Error reading attribute"));
+                    });
+
+                    if dir_queue.is_empty() {
+                        txml_structure.add_file(file)
+                    } else {
+                        dir_queue
+                            .back_mut()
+                            .expect("Shouldn't be empty")
+                            .add_file(file)
+                    }
+                }
+                _ => return Err(InvalidTag),
+            },
+            Ok(Event::Text(e)) => {
+                if let Some(ref mut file) = current_file {
+                    let content = String::from_utf8_lossy(&e).to_string();
+                    if content.replace(" ", "").is_empty() {
+                        continue;
+                    }
+
+                    file.set_text(content);
+
+                    if dir_queue.is_empty() {
+                        txml_structure.add_file(current_file.unwrap())
+                    } else {
+                        dir_queue
+                            .back_mut()
+                            .expect("Shouldn't be empty")
+                            .add_file(current_file.unwrap())
+                    }
+
+                    current_file = None;
+                }
+            }
+            Ok(Event::End(ref e)) => match e.name().0 {
+                b"Root" => break,
+                b"Directory" => {
+                    dir_queue.pop_back();
+                }
+                b"File" => current_file = None,
+                _ => return Err(InvalidTag),
+            },
+            Ok(Event::Eof) => break,
+            Err(_e) => return Err(UnknownParseError),
+            _ => (),
+        }
+
+        event_buf.clear();
+    }
+
+    Ok(txml_structure)
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+    use crate::objects::Instantiable;
+    use std::str::FromStr;
+
+    #[test]
+    fn process_txml_test()
+    {
+        let biding = &PathBuf::from_str(
+            "/home/borja/projects/mk-template/crates/txml_processor/template_example.xml",
+        )
+        .expect("Should exist");
+        let txml = process_txml(biding).expect("TODO: panic message");
+
+        txml.instantiate(
+            &PathBuf::from_str("/home/borja/projects/mk-template/test").expect("Should exist"),
+        );
+    }
+}
---
+++ crates/txml_processor/src/objects.rs
@@ -0,0 +1,296 @@
+use crate::objects::CommandError::{CommandFailed, CreationError, InvalidInput};
+use quick_xml::events::attributes::Attribute;
+use std::fmt::{Debug, Formatter};
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Command;
+// region: Generics
+
+pub enum CommandError
+{
+    CommandFailed,
+    InvalidInput,
+    CreationError,
+}
+
+impl Debug for CommandError
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result
+    {
+        match self {
+            CommandFailed => write!(f, "Command execution failed."),
+            InvalidInput => write!(f, "Command input was invalid."),
+            CreationError => write!(f, "Command creation failed."),
+        }
+    }
+}
+
+pub trait AttributeHandler
+{
+    fn process_attribute(&mut self, attribute: Attribute);
+}
+
+pub trait Instantiable
+{
+    fn instantiate(&self, dir: &PathBuf);
+}
+
+// endregion: Generics
+
+// region: Directory
+
+pub struct Directory
+{
+    name: String,
+    out_command: String,
+    in_command: String,
+    files: Vec<File>,
+    directories: Vec<Directory>,
+}
+
+impl Directory
+{
+    pub fn new() -> Directory
+    {
+        Directory {
+            name: String::new(),
+            out_command: String::new(),
+            in_command: String::new(),
+            files: Vec::new(),
+            directories: Vec::new(),
+        }
+    }
+
+    pub fn add_file(&mut self, file: File)
+    {
+        self.files.push(file);
+    }
+
+    pub fn add_directory(&mut self, directory: Directory)
+    {
+        self.directories.push(directory);
+    }
+}
+
+impl Instantiable for Directory
+{
+    fn instantiate(&self, dir: &PathBuf)
+    {
+        let dir_name = format!("{}", self.name);
+        let new_path_buff = dir.join(&dir_name);
+
+        if new_path_buff.exists() {
+            println!("Directory {} already exist. Skipping creation.", dir_name);
+            return;
+        }
+
+        fs::DirBuilder::new()
+            .create(&new_path_buff)
+            .expect("Error creating directory");
+
+        if !self.in_command.is_empty() {
+            let command_execution = execute_command(&self.in_command, &new_path_buff);
+
+            match command_execution {
+                Err(e) => println!("File {} created but the command failed: {e:?}", dir_name),
+                _ => (),
+            }
+        }
+
+        if !self.out_command.is_empty() {
+            let command_execution = execute_command(&self.out_command, dir);
+
+            match command_execution {
+                Err(e) => println!("File {} created but the command failed: {e:?}", dir_name),
+                _ => (),
+            }
+        }
+
+        self.files.iter().for_each(|file| file.instantiate(&dir));
+
+        self.directories
+            .iter()
+            .for_each(|directory| directory.instantiate(dir));
+    }
+}
+
+impl AttributeHandler for Directory
+{
+    fn process_attribute(&mut self, attribute: Attribute)
+    {
+        match attribute.key.0 {
+            b"name" => {
+                self.name = String::from_utf8_lossy(&attribute.value).to_string();
+            }
+            b"in_command" => {
+                self.in_command = String::from_utf8_lossy(&attribute.value).to_string()
+            }
+            b"out_command" => {
+                self.out_command = String::from_utf8_lossy(&attribute.value).to_string()
+            }
+            _ => println!(
+                "Unknown attribute for Directory: {}",
+                String::from_utf8_lossy(attribute.key.0)
+            ),
+        }
+    }
+}
+
+// endregion: Directory
+
+// region: File
+
+pub struct File
+{
+    name: String,
+    extension: String,
+    command: String,
+    content: String,
+}
+
+impl File
+{
+    pub fn new() -> File
+    {
+        File {
+            name: String::new(),
+            extension: String::new(),
+            command: String::new(),
+            content: String::new(),
+        }
+    }
+
+    pub fn set_text(&mut self, text: String)
+    {
+        self.content = text;
+    }
+}
+
+impl Instantiable for File
+{
+    fn instantiate(&self, dir: &PathBuf)
+    {
+        let file_name = if self.extension.is_empty() {
+            format!("{}", self.name)
+        } else {
+            format!("{}.{}", self.name, self.extension)
+        };
+        let new_path_buff = dir.join(&file_name);
+
+        if new_path_buff.exists() {
+            println!("File {} already exist. Skipping creation.", file_name);
+            return;
+        }
+
+        fs::File::create(&new_path_buff)
+            .expect("Error creating file")
+            .write_all(self.content.as_bytes())
+            .expect("Error writing to file");
+
+        if !self.command.is_empty() {
+            let command_execution = execute_command(&self.command, dir);
+
+            match command_execution {
+                Err(e) => println!("File {} created but the command failed: {e:?}", file_name),
+                _ => (),
+            }
+        }
+    }
+}
+
+impl AttributeHandler for File
+{
+    fn process_attribute(&mut self, attribute: Attribute)
+    {
+        match attribute.key.0 {
+            b"name" => {
+                self.name = String::from_utf8_lossy(&attribute.value).to_string();
+            }
+            b"extension" => {
+                self.extension = String::from_utf8_lossy(&attribute.value).to_string();
+            }
+            b"command" => {
+                self.command = String::from_utf8_lossy(&attribute.value).to_string();
+            }
+            _ => println!(
+                "Unknown attribute for File: {}",
+                String::from_utf8_lossy(attribute.key.0)
+            ),
+        }
+    }
+}
+
+// endregion: File
+
+// region: TxmlStructure
+
+pub struct TxmlStructure
+{
+    files: Vec<File>,
+    directories: Vec<Directory>,
+}
+
+impl TxmlStructure
+{
+    pub fn new() -> TxmlStructure
+    {
+        TxmlStructure {
+            files: Vec::new(),
+            directories: Vec::new(),
+        }
+    }
+
+    pub fn add_file(&mut self, file: File)
+    {
+        self.files.push(file);
+    }
+
+    pub fn add_directory(&mut self, directory: Directory)
+    {
+        self.directories.push(directory);
+    }
+}
+
+impl Instantiable for TxmlStructure
+{
+    fn instantiate(&self, dir: &PathBuf)
+    {
+        self.files.iter().for_each(|file| file.instantiate(&dir));
+
+        self.directories
+            .iter()
+            .for_each(|directory| directory.instantiate(dir));
+    }
+}
+
+// endregion: FxmlStructure
+
+// region: Utils
+
+fn execute_command(command: &String, dir: &PathBuf) -> Result<(), CommandError>
+{
+    let command_parts: Vec<&str> = command.split_whitespace().collect();
+
+    if command_parts.is_empty() {
+        return Err(InvalidInput);
+    }
+
+    let cmd = command_parts[0];
+    let args = &command_parts[1..];
+
+    let status = Command::new(cmd).current_dir(dir).args(args).status();
+
+    let status = match status {
+        Ok(s) => s,
+        Err(_) => return Err(CreationError),
+    };
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(CommandFailed)
+    }
+}
+
+// endregion: Utils
