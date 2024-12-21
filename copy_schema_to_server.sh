81dc22d91973f28ea9b0e0466b951f26d47ed39e
Borja Castellano <borjacastellano1@gmail.com>
2024-12-21 19:14:29 +0100

    Changed the copy_schema_to_server.sh script to use the new server location

--- copy_schema_to_server.sh
+++ copy_schema_to_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp crates/txml_processor/txml_schema.xsd admin@10.8.0.1:/var/www/lebastudios.org/xml-schemas
+scp crates/txml_processor/txml_schema.xsd root@lebastudios.org:/var/www/lebastudios.org/xml-schemas
