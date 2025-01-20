5eaf23d927db9eb271ab89414753a01ede1d5811
Borja Castellano <borjacastellano1@gmail.com>
2025-01-20 07:37:57 -0500

    Revert "fixed a typo in txml structure"
    This reverts commit b0c254a3964155a7e69cb79f17b6d1b290d1f896.

--- crates/txml_processor/src/txml_structure.rs
+++ crates/txml_processor/src/txml_structure.rs
@@ -19,7 +19,7 @@ pub struct TxmlStructure {
     files: Vec<File>,
     directories: Vec<Directory>,
     metadata: TemplateMetadata,
-    renameable: bool,
+    renamable: bool,
 }
 
 impl TxmlStructure {
@@ -28,7 +28,7 @@ impl TxmlStructure {
             files: Vec::new(),
             directories: Vec::new(),
             metadata: TemplateMetadata::new(),
-            renameable: true,
+            renamable: true,
         }
     }
 
@@ -130,7 +130,7 @@ impl TxmlStructure {
 impl AttributeHandler for TxmlStructure {
     fn process_attribute(&mut self, attr: Attribute) {
         match attr.key.0 {
-            b"renameable" => self.renameable = String::from_utf8_lossy(&attr.value).to_string() == "true",
+            b"renamable" => self.renamable = String::from_utf8_lossy(&attr.value).to_string() == "true",
             _ => (),
         }
     }
@@ -319,7 +319,7 @@ impl Instantiable for TxmlStructure {
     }
 
     fn instantiate_with_name(&self, dir: &PathBuf, name: &str) {
-        if self.files.len() + self.directories.len() > 1 || !self.renameable {
+        if self.files.len() + self.directories.len() > 1 || !self.renamable {
             self.instantiate(dir);
             return;
         }
